e723b0519b35bd9f03017a3793d22dba004b5034
erickadikah <erickadikah2030@gmail.com>
2022-08-23 08:21:03 +0000

    cool work

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,69 +1,63 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * _realloc - Reallocates a memory block
- * @ptr: The pointer to the previous memory block
- * @old_size: The size of the old memory block
- * @new_size: The size of the new memory block
- *
- * Return: The pointer to the new memory block otherwise NULL
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+size_t print_listint(const listint_t *h)
 {
-	void *new_ptr;
-	unsigned int min_size = old_size < new_size ? old_size : new_size;
-	unsigned int i;
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr != NULL)
+	current = h;
+	n = 0;
+	while (current != NULL)
 	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		new_ptr = malloc(new_size);
-		if (new_ptr != NULL)
-		{
-			for (i = 0; i < min_size; i++)
-				*((char *)new_ptr + i) = *((char *)ptr + i);
-			free(ptr);
-			return (new_ptr);
-		}
-		free(ptr);
-		return (NULL);
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
 	}
-	else
-	{
-		new_ptr = malloc(new_size);
-		return (new_ptr);
-	}
+
+	return (n);
 }
 
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: pointer to a pointer of the start of the list
+ * @n: integer to be included in node
+ * Return: address of the new element or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
 
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
 
 /**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: list head
- * Return: 0 if no, 1 if yes
+ * free_listint - frees a listint_t list
+ * @head: pointer to list to be freed
+ * Return: void
  */
-
-int check_cycle(listint_t *list)
+void free_listint(listint_t *head)
 {
-	listint_t *fast, *slow = list;
+	listint_t *current;
 
-	if (list == NULL)
-		return (0);
-
-	fast = list->next;
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	while (head != NULL)
 	{
-		if (slow == fast)
-			return (1);
-		fast = fast->next->next;
-		slow = slow->next;
+		current = head;
+		head = head->next;
+		free(current);
 	}
-	return (0);
 }
