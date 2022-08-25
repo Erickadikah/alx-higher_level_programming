f52cceff46a7a107c6032154a5aee53bfdd1269b
erickadikah <erickadikah2030@gmail.com>
2022-08-25 12:05:14 +0000

    calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if__name__ == "__main__"
+
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print('{:d} + {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d}'.format(a, b, sub(a, b))) 
+    print('{:d} * {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d}'.format(a, b, div(a, b)))
