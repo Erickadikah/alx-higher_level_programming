ce093c6167746f5082ad940e51dbc2573bee3c36
erickadikah <erickadikah2030@gmail.com>
2022-08-28 14:52:09 +0000

    initial commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-
-
 def element_at(my_list, idx):
-    if (idx < 0) or (idx > len(my_list) - 1):
-        return None
-    return(my_list[idx]
+    return(my_list[idx] if 0 <= idx < len(my_list) else "None")
