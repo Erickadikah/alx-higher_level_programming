11629e99b47f5cd63edaa8af7ed2a18c6d26d29b
erickadikah <erickadikah2030@gmail.com>
2022-08-29 20:29:14 +0000

    nem line of code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new_str += i
-    return (new_str)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
