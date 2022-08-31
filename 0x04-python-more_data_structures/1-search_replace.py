cab1459716a15d984afd2f878c2fa068fb5e4920
erickadikah <erickadikah2030@gmail.com>
2022-08-31 09:32:17 +0000

    tast two

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+#def search_replace(my_list, search, replace):
+def uniq_add(my_list=[]):
+    result = 0
+    for x in set(my_lists):
+        result += x
+    return result
