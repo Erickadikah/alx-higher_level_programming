78bc686497b75d7926d10db6d64fbad47ad59ba3
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-08-31 12:36:02 +0300

    new code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
-#def search_replace(my_list, search, replace):
-def uniq_add(my_list=[]):
+def search_replace(my_list, search, replace):
+ def uniq_add(my_list=[]):
     result = 0
     for x in set(my_lists):
         result += x
