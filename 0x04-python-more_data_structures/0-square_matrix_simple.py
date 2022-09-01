d87c1078469a9a5519dc41ecca299d213d2a951c
erickadikah <erickadikah2030@gmail.com>
2022-09-01 08:51:31 +0000

    quick fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
-
 def square_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+    new_matrix = [[x ** 2 for x in row] for row in matrix]
+    return new_matrix
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
-
-def uniq_add(my_list=[]):
-    result = 0
-    for x in set(my_list):
-        result += x
-    return result
+def search_replace(my_list, search, replace):
+    copy = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copy.append(replace)
+        else:
+            copy.append(my_list[i])
+    return copy
