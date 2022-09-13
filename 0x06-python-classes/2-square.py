3c4e12d0d3a46fdef6b62d87c582aa955368474f
erickadikah <erickadikah2030@gmail.com>
2022-09-13 22:18:54 +0000

    defines a sqaure

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+"""docstring for square"""
+
+
+class Square:
+    """docstring for init"""
+    def __init__(self, size=0):
+        if isinstance(size, int) and size >= 0:
+
+            self.__size = size
+        elif not isinstance(size, int):
+            raise TypeError("size must be an interger")
+        elif size > 0:
+            raise ValueError("size must be >= 0")
