6b74fe948a9b8dafb5cdf4febac4188265e3cb5d
erickadikah <erickadikah2030@gmail.com>
2022-09-21 05:16:37 +0000

    py file

---
+++ 0x07-python-test_driven_development/python.h
@@ -0,0 +1,14 @@
+#ifndef _PYTHON_H
+#define _PYTHON_H
+
+/**
+* print_python_string - prints information about python strings.
+* @p: A pyObject string object.
+*/
+{
+
+void print_python_string(pyobject *p);
+
+#endif
+
+}
