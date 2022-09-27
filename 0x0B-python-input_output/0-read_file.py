80e47ff1ca3806c80f9e9c9bc783081d1d2e9e16
erickadikah <erickadikah2030@gmail.com>
2022-09-27 11:33:07 +0000

    bug fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """
     reads a text file (UTF8) and prints it to stdout
     """
-    with open("filename", encoding="utf-8") as f:
-        print(f.read())
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
