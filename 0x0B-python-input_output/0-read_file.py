58e978eeb97c738704cd7c1ee7301dbc1a0f9e2e
erickadikah <erickadikah2030@gmail.com>
2022-09-27 11:27:59 +0000

    stout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+def read_file(filename=""):
+
+    """
+    reads a text file (UTF8) and prints it to stdout
+    """
+    with open("filename", encoding="utf-8") as f:
+        print(f.read())
