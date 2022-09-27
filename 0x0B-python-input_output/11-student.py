801bba1d924ccfbce13b8d1b145322a06752f02d
erickadikah <erickadikah2030@gmail.com>
2022-09-27 20:30:58 +0000

    more tasks

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Student to JSON """
+
+
+class Student:
+    """ New class student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student instance """
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            dic = {}
+            for nm in attrs:
+                if hasattr(self, nm):
+                    dic[nm] = getattr(self, nm)
+            return (dic)
+
+    def reload_from_json(self, json):
+<<<<<<< HEAD
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except:
+                pass
+
+=======
+        """ Replaces all attributes of the Student instance"""
+        save = vars(self)
+        for key, value in json.items():
+            save[key] = value
+>>>>>>> d409088cfccd9003c34ef281eb61813cbf843c38
