da669e367774324412f944727b462c807ad831e1
erickadikah <erickadikah2030@gmail.com>
2022-09-28 06:22:44 +0000

    task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,38 +1,22 @@
 #!/usr/bin/python3
-""" pascal_triangle module """
+"""pascal's Triangle module"""
 
 
-def pascal_triangle(prmN):
+def pascal_triangle(n):
+    """Function that returns a list of
+    lists of integers representing the
+    Pascal's triangle of n
     """
-        Function that define a Pascal triangle
-        Args:
-            prmN: size of the triangle
-    """
 
-    triangle = []
-    if prmN <= 0:
-        return triangle
+    if n <= 0:
+        return []
 
-    for i in range(prmN):
-        row = []
-        for j in range(prmN):
-            if (j > i):
-                break
-            if i > 0 and len(triangle) == i and len(triangle[i - 1]) > j:
-                y = triangle[i - 1][j]
-            elif len(triangle) >= i:
-                y = 0
-            if (
-                i > 0 and len(triangle) > i - 1 and
-                j > 0 and len(triangle[i - 1]) > j - 1
-            ):
-                x = triangle[i - 1][j - 1]
-            else:
-                x = 0
-            if x == 0 and y == 0:
-                row.append(1)
-            else:
-                row.append(x + y)
+    triangle = [[1]]
+
+    for x in range(1, n):
+        row = [1]
+        for y in range(1, x):
+            row.append(triangle[x-1][y-1] + triangle[x-1][y])
+        row.append(1)
         triangle.append(row)
-
-    return 
+    return triangle
