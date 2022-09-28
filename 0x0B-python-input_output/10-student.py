455a861b1b3948890b657c48a7b2b0a7dbeb0a5f
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-09-28 10:29:12 +0300

    bug fix

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
 class Student:
+    """Represents a student."""
+
     def __init__(self, first_name, last_name, age):
+        """Initializes a new student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-
-        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+    def to__json(self, attrs=None):
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-        
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            seattr(self, k, v)
