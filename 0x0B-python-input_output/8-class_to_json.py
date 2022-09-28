c96d6e02de9e6965242386f6e8c706004187de30
erickadikah <erickadikah2030@gmail.com>
2022-09-28 12:33:59 +0000

    error fixed

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """
-contains the "class_to json" function 
+contains the "class_to json" function
 """
 
 
 def class_to_json(obj):
-    """returrns the dictionarty description with simple data structure 
-    (list, dictionary, sting, interger and boolean)
-    for JSON serialization of an object
-    """
+    """Return the dictionary representation of a simple data structure."""
     return obj.__dict__
