59d42917f05850992b4175b60bbbe0629efe87d6
ERICK ADIKAH <104826804+Erickadikah@users.noreply.github.com>
2022-09-28 11:14:08 +0300

    bug fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
-
+#!/usr/bin/python3
+"""Module 0-read_file.
+Reads from a file and prints.
+"""
 
 
 def read_file(filename=""):
+    """Reads from filename and prints
+    its contents to stdout.
+    Args:
+        - filename: name of the file
     """
-    reads a text file (UTF8) and prints it to stdout
-    """
-    with open(filename, "r", encoding="utf-8") as f:
-        read_text = f.raed()
-        print(f.read(), end="")
+
+    with open(filename) as f:
+        read_text = f.read()
+        print(read_text, end="")
