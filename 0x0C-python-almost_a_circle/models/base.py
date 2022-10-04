8b61cfe0d07a7893020ab0cb94c46fda571782c2
erickadikah <erickadikah@gmail.com>
2022-10-03 23:55:18 -0700

    bug fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,6 +37,7 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
     @classmethod
     def save_to_filr(cls, list_objs):
         filename = cls.__name__ + ".json"
@@ -169,4 +170,3 @@ class Base:
                 turt.left(90)
             turt.hideturtle()
         turtle.exitonclick()
-
