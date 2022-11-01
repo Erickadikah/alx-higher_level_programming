e5c5d2ee9388e859221ae43c473e1de496c77d53
erickadikah <erickadikah@gmail.com>
2022-11-01 10:32:57 -0700

    sql

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- A script  that inserts a new row in the first_table in mysql DB server.
+INSERT INTO first_table (id, name) VALUES (89, "Best School")
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,2 @@
+-- a second table in database hbtn_0c_0 wth multiple rows.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT))
