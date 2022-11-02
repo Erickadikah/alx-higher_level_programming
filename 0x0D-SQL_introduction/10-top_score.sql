344865e5f9bda9c4cea0a42c85b001da48dc1065
erickadikah <erickadikah@gmail.com>
2022-11-02 04:58:13 -0700

    new

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
---  a script that lists all records of the table second_table 
+-- script that lists all records of the table second_table 
 -- Records are ordered by descending score
-SELECT* `score`, `name`
+SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
