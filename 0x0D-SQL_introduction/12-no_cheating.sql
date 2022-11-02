8a57256ff25f4a1312b18a585748df0aa9bbb731
erickadikah <erickadikah@gmail.com>
2022-11-02 03:39:24 -0700

    new

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
 -- a script that updates the score of bob to 10 in table second_table.
-UPDATE 'second_table'
-SET 'score' = 10
-WHERE 'name' = "Bob";
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
