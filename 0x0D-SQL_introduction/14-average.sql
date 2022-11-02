c25983a00c97a9c8bc36427ce75598c51f863137
erickadikah <erickadikah@gmail.com>
2022-11-02 05:02:57 -0700

    newone

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,3 @@
--- computing the score avarage of all records in table seceond_table
--- in hbtn_0c_0 in my MYSQL server
-SELECT AVG(`score`) AS `avarage`
+-- Computes the average score of all records in the table second_table.
+SELECT AVG(`score`) AS `average`
 FROM `second_table`;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- a script that lists the number of records with the same score in the table second_table
 -- in database hbtn_0c_0 in MYsql server
-SELECT `score` COUNT(*) AS `number`
+SELECT `score`, COUNT(*) AS `number`
 GROUP BY `score`
 ORDER BY `number` DESC;
