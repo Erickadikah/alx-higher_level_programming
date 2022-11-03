e51dfd781828e02f7f5e3b86ce68b71fd095b38b
erickadikah <erickadikah@gmail.com>
2022-11-03 05:24:29 -0700

    grant

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- creates in my MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' INDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
-FUSH PRIVILAGES;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
+INDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILAGES 
+ON *.* TO 'user_0d_1'@'localhost';
