1608473eef90ea09d067dbdf8f9363a4c3a00575
erickadikah <erickadikah@gmail.com>
2022-11-03 10:56:23 -0700

    less

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+--Creates a database hbtn_0d_usa and the table states in my Sql sever
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	PRIMARY KEY (`id`),
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(256) NOT NULL);
