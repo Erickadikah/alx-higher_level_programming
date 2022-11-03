742e9e9502d10a3bb07f4db8c0f5d90f6c5fcbd5
erickadikah <erickadikah@gmail.com>
2022-11-03 09:25:05 -0700

    new

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Script that creats the table unique_id on my SQL server.
-CREATE TABLE IF NOT EXISTS  unique_id (id INT DEFAULT VALUE 1 UNIQUE, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS  unique_id (id INT DEFAULT UNIQUE 1, name VARCHAR(256) NOT NULL);
