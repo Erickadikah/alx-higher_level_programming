136165c36e3a5d5c7eb198180746597f396e87ea
erickadikah <erickadikah@gmail.com>
2022-11-03 09:09:16 -0700

    new

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- script that creates the table force_name on my SQL server.
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- Script that creates the table id_not_null on my  MySQL server.
+CREATE TABLE IF NOT EXISTS id_not_null (id INT, name VARCHAR(256) NOT NULL);
