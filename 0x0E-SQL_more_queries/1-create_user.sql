533ed21bc4c24551b42566484610956290999f89
erickadikah <erickadikah@gmail.com>
2022-11-03 05:31:38 -0700

    new

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates in my MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
-INDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES 
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 
 ON *.* TO 'user_0d_1'@'localhost';
