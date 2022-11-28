8a9eeddbff77fcae5907231453bf4f3676314fa5
erickadikah <erickadikah2030@gmail.com>
2022-11-28 15:38:47 +0300

    new

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const count = process.argv.lenth;
-console.log(count === 2 ? 'No argment' : count === 3 ? 'Argument found' : 'Arguments found');
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
 
