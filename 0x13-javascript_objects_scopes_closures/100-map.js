daabf1db75acdc184a949b78730ae297fd6ace07
erickadikah <erickadikah2030@gmail.com>
2022-11-29 23:40:16 +0300

    new

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-const map1 = list.map(x => x * 2);
-console.log(map1);
+console.log(list.map((item, index) => item * index));
