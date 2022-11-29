7a13bd9cf5e437c8bfed9c4296a06480a6bb7ed0
erickadikah <erickadikah2030@gmail.com>
2022-11-29 20:48:36 +0300

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-exports.esrever = function (list)
-{
-	const size = list.length;
-	let reversed = [];
-	for (let i = size - 1; i >= 0; i--) {
-		reversed.push(list[i]);
-	}
-	return reversed
+exports.esrever = function (list) {
+  const size = list.length;
+  const reversed = [];
+  for (let i = size - 1; i >= 0; i--) {
+    reversed.push(list[i]);
+  }
+  return reversed;
 };
