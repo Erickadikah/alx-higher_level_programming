ecbf85adf419ef51d78d5aa4e37e5160268fbb60
erickadikah <erickadikah2030@gmail.com>
2022-11-29 19:54:09 +0300

    new

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-	let x = searchElement
-	let n = list.length
-        let res = 0;
-        for (let i=0; i<n; i++)
-        {
-            if (x == list[i])
-                res++;
-        }
-        return res;
-    }
+  const x = searchElement;
+  const n = list.length;
+  let res = 0;
+  for (let i = 0; i < n; i++) {
+    if (x === list[i]) { res++; }
+  }
+  return res;
+};
