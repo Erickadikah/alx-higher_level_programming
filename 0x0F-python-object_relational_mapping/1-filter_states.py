6f1357589b3b75658d6027dacaee6706d6fb3e92
erickadikah <erickadikah2030@gmail.com>
2022-12-02 20:14:42 +0300

    error check

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,8 +9,8 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user="root",
-                         passwd=sys.argv[2], port=3306, db="hbtn_0e_0_usa")
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], port=3306, db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` ORDER BY id ASC")
     query_rows = cur.fetchall()
