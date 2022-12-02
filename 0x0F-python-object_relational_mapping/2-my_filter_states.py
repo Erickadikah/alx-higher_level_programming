e509a0692effa1a4141dba216f5cf78391b50872
erickadikah <erickadikah2030@gmail.com>
2022-12-02 20:24:04 +0300

    error check

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,14 +7,13 @@ whose name matches that supplied as argument.
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user="root", passwd=sys.argv[2], port=3306, db = "hbtn_0e_0_usa")
-
 if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], port=3306, db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE'{:s}'ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE'{:s}'\
+            ORDER BY id ASC".format(sys.argv[4]))
     states = cur.fetchall()
     for state in states:
         if state[1] == sys.argv[4]:
             print(state)
-cur.close()
-db.close()
