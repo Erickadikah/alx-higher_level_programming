23507a89b5b426b1e2caaee294bf3d84c5c3a795
erickadikah <erickadikah2030@gmail.com>
2022-12-02 21:46:51 +0300

    indent bug

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+"""
+ a script that lists all cities from the database hbtn_0e_4_usa
+take 3 arguments: mysql username, mysql password and database
+name
+"""
 import sys
 import MySQLdb
 
-
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],db=sys.argv[3], port=3306)
-
 if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT `cities`.`id`,`cities`.`name`, `states`.`name`\
-            FROM `cities` INNER JOIN `states` ON `cities`.`state_id` = `states`.`id`\
-            ORDER BY `cities`.`id`")
+            FROM `cities` INNER JOIN `states` ON `cities`.`state_id`\
+            = `states`.`id` ORDER BY `cities`.`id`")
     [print(city) for city in cur.fetchall()]
 
 cur.close()
