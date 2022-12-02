5c19accdea1596ca5ffeca9c576ab95e2aa1f75c
erickadikah <erickadikah2030@gmail.com>
2022-12-02 15:56:38 +0300

    progress

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,13 +9,8 @@ take 3 arguments<mysql username>
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],db=sys.argv[3], port=3306)
-
 if __name__ == "__main__":
-    cur = db.cursor()
-    cur.execute("USE `hbtn_0e_0_usa`")
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],db=sys.argv[3], port=3306)
+    c = db.cursor()
     cur.execute("SELECT * FROM `states`")
     [print(state) for state in cur.fetchall()]
-
-cur.close()
-db.close()
