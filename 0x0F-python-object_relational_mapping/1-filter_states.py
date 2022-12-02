6a02214521f7da6f40b0fdd44a678b1e28c0498e
erickadikah <erickadikah2030@gmail.com>
2022-12-02 20:01:54 +0300

    new

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+a script that takes in an argument and displays 
+all values in the states table of hbtn_0e_0_usa where name matches the argument.
+
+"""
 import sys
 import MySQLdb
 
@@ -11,6 +16,3 @@ if __name__ == "__main__":
     for row in query_rows:
         if row[1][0] == "N":
             print(row)
-
-cur.close()
-db.close()
