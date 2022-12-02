898030842466d33619b55b03cce3618095657fd0
erickadikah <erickadikah2030@gmail.com>
2022-12-02 18:40:25 +0300

    progress

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,16 +5,12 @@ whose name matches that supplied as argument.
 
 """
 import sys
-from sys import argv
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", user="root", passwd=sys.argv[2], port=3306, db="hbtn_0e_4_usa")
-
 if __name__ == "__main__":
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE {:s} ORDER BY id ASC", argv[4],)
-    rows = cur.fetchall()
-    for cities in rows:
-            print(cities)
-cur.close()
-db.close()
+    db = MySQLdb.connect(host="localhost", user="root", passwd=sys.argv[2], port=3306, db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` INNER JOIN `states`\
+            ON  `cities`.`state_id` = `states`.`id` \
+            ORDER BY `cities`.`id`")
+    print(", ".join([ct[2] for ct in  c.fetchall() if ct[4] == sys.argv[4]]))
