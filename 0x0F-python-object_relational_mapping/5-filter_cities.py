98cd1d59ddae69bdbdc75c77e148013a89e982ff
erickadikah <erickadikah2030@gmail.com>
2022-12-02 18:46:30 +0300

    instance

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user="root", passwd=sys.argv[2], port=3306, db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `cities` INNER JOIN `states`\
+    c.execute("SELECT * FROM `cities` INNER JOIN `states` \
             ON  `cities`.`state_id` = `states`.`id` \
             ORDER BY `cities`.`id`")
     print(", ".join([ct[2] for ct in  c.fetchall() if ct[4] == sys.argv[4]]))
