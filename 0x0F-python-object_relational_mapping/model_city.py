22ee31a670a7efa5cc3340495cb1d7ecf6ec7eda
erickadikah <erickadikah2030@gmail.com>
2022-12-04 20:11:34 +0300

    new

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+defines a state model.
+inherits from SQLAlchemy Base and links to the MySQL table states.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """represents a state for a MySQL database."""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(128), nullable=False)
