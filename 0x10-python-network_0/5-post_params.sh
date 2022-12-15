2c5838d433adc476c31dbd76da0163e5faab1bc8
erickadikah <erickadikah2030@gmail.com>
2022-12-15 21:43:37 +0300

    displays email and subjects

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script takes URL and sends POST request and displays body response
-curl -s -X POST -d "email=test@gmail.com, subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
