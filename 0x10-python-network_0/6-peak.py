81afedac71e6eae02a9738f03b8ef5ef822c6a63
erickadikah <erickadikah2030@gmail.com>
2022-12-16 12:01:34 +0300

    bug

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,12 @@
 #!/usr/bin/pyhon3
-"""afuction that finds a peak in unsorted list
+"""afuction that finds a peak in
+    alist of unsorted intergers
 """
 
 
 def find_peak(list_of_integers):
     """function that finds peak in a list"""
-    if len(list_of_integers) == 0:
+    if list_of_integers == []:
         return None
     list_of_integers.sort()
     return list_of_integers[-1]
