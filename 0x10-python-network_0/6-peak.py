7c0f2cab0a9621231482064a168e28541cf134d6
erickadikah <erickadikah2030@gmail.com>
2022-12-16 11:30:36 +0300

    interview question

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,11 @@
 #!/usr/bin/pyhon3
-"""afuction that finds a peak in
-    alist of unsorted intergers
+"""afuction that finds a peak in unsorted list
 """
 
 
 def find_peak(list_of_integers):
     """function that finds peak in a list"""
-    if list_of_integers == []:
+    if len(list_of_integers) == 0:
         return None
     list_of_integers.sort()
     return list_of_integers[-1]
