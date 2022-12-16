6d36a17e700cb00bd553ccdfe88a8a39e1bc4b3d
erickadikah <erickadikah2030@gmail.com>
2022-12-16 15:12:08 +0300

    network1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://alx-intranet.hbtn.io/status
+Module for fetching the status of https://alx-intranet.hbtn.io/status
 """
 
+import urllib.request
 
-if __name__ == '__main__':
-    import urllib.request
+def get_status():
+    """
+    Fetches the status of https://alx-intranet.hbtn.io/status
+    and returns the body of the response as a bytes object
+    """
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        return response.read()
 
-    with urllib.request.urlopen \
-            ('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    # Get the status and print the body of the response
+    status = get_status()
+    print("Body response:")
+    print("\t- type:", type(status))
+    print("\t- content:", status)
+    print("\t- utf8 content:", status.decode('utf-8'))
