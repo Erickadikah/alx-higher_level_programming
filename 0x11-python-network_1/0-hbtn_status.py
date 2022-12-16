091dc50e842550bf4116a1abdd4bcfd2951fa13c
erickadikah <erickadikah2030@gmail.com>
2022-12-16 15:08:47 +0300

    network1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen\
+    with urllib.request.urlopen \
             ('https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id
+"""
 import sys
 import urllib.request
 
