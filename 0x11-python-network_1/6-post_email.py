156ae4affe9150fd11d1ed6642eddee76cb86f75
erickadikah <erickadikah2030@gmail.com>
2022-12-16 18:12:03 +0300

    post request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+-> Python script that takes in a URL and an email, sends
+-> a POST request to the passed URL with the email as a parameter,
+-> and displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+
+    request = requests.post(url, data=value)
+    print(request.text)
