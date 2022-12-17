91ed3a93eb5112ef2b3b8cf40041d1f244054d41
erickadikah <erickadikah2030@gmail.com>
2022-12-17 13:35:45 +0300

    fixed

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
 
-    request = requests.post(url, data=value)
-    print(request.text)
+    r = requests.post(url, data=value)
+    print(r.text)
