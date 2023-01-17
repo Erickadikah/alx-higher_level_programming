f4909c4e01ab2a15d91b9fc65b4b5f9843999a2c
erickadikah <erickadikah2030@gmail.com>
2023-01-18 00:30:17 +0300

    task five

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
-
+// gets content of a webpage and
+// stores it in a file
 const fs = require('fs');
 const request = require('request');
-
 
 const url = process.argv[2];
 const fileName = process.argv[3];
 
-request.get(url,
-		(error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        fs.writeFile(fileName, body, (err) => {
-            if (err) throw err;
-            console.log(`Webpage saved to ${fileName}`);
-        });
-    } else {
-        console.log(`Error: ${error}`);
-    }
+request.get(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(fileName, body, (err) => {
+      if (err) console.error(err);
+    });
+  } else {
+    console.log(error);
+  }
 });
