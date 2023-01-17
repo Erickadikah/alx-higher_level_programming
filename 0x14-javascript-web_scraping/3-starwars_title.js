1bc874aa7e0cfb16d435b9dab63fa4ed59f28d16
erickadikah <erickadikah2030@gmail.com>
2023-01-17 23:53:42 +0300

    task 3 correction

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,9 @@ const req = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-req.get(url,
-  function (err, res, body) {
-    jsonBody = JSON.parse(body);
-    console.log(jsonBody.title);
+req.get(url, function (err, response, body) {
+  if (err) console.error(err); else {
+    const jsonbody = JSON.parse(body);
+    console.log(jsonbody.title);
   }
-);
+});
