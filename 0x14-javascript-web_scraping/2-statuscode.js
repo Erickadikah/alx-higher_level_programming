00eff1b3402832f11d591354e249e13b38b3703c
erickadikah <erickadikah2030@gmail.com>
2023-01-17 23:42:26 +0300

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
+// script displays the status code of a Get request
 
 const req = require('request');
-const API_URL = process.argv[2];
+const url = process.argv[2];
 
-req.get(API_URL, function (error, response, body) {
-  code = response.statusCode;
-  console.log(`code: ${code}`);
+req.get(url, function (err, response, body) {
+  if (err) console.error(err); else {
+    const code = response.statusCode;
+    console.log(`code: ${code}`);
+  }
 });
